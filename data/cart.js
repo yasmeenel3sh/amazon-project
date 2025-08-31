cfcf1d4137b83a56dbe8f6bbeaea47e8a2907ecd
yasmeenel3sh <yasmeenel3sh@gmail.com>
2025-08-31 06:08:11 +0300

    fix price formatting and radio selectors

--- data/cart.js
+++ data/cart.js
@@ -1,7 +1,12 @@
 export const cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 3
-}];
+},
+{
+  productId: 'bc2847e9-5323-403f-b7cf-57fde044a955',
+  quantity: 3
+}
+];
 
 export function addToCart(productId){
     let matchingItem;
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,8 +2,8 @@
 // order in html
 
 import {cart, addToCart} from '../data/cart.js';
-
 import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 let productsHTML = '';
 
@@ -28,7 +28,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-price">
-        $${(product.priceCents / 100).toFixed(2)}
+        $${formatCurrency(product.priceCents)}
       </div>
 
       <div class="product-quantity-container">
