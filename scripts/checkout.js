f6aff85434842857c3e5bb80233fa0c5084cd644
yasmeenel3sh <yasmeenel3sh@gmail.com>
2025-08-31 06:37:25 +0300

    delete products from html using delete button

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,7 +16,7 @@ cart.forEach((cartItem) => {
     }
   })
 cartSummaryHTML +=
-`   <div class="cart-item-container">
+`   <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -39,7 +39,7 @@ cartSummaryHTML +=
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary js-delete-link 
+            <span class="delete-quantity-link link-primary js-delete-link" 
             data-product-id="${matchingProduct.id}">
               Delete
             </span>
@@ -103,6 +103,12 @@ cartSummaryHTML +=
         const productId = link.dataset.productId;
 
         removeFromCart(productId);
+        console.log(productId);
+
+        const container = document.querySelector(
+          `.js-cart-item-container-${productId}`
+        )
+        container.remove();
       });
 
     });
